30083c02cbaacc01ff070b7d6c11d625f94e7eb9
GabrielBdSilva <gabriel.sites3200@gmail.com>
2024-03-02 13:19:10 -0300

    inicialização do projeto com spring initalizer

---
+++ src/main/java/com/example/FilmLibrary/FilmLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.FilmLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmLibraryApplication.class, args);
+	}
+
+}
