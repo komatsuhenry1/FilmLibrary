f5b6027b1a524094c2954e55181bcf128ac84504
gabrieldbsilva <gabriel.sites3200@gmail.com>
2024-05-15 11:13:57 -0300

    deploy config

--- src/main/java/com/example/FilmLibrary/controller/AtorController.java
+++ src/main/java/com/example/FilmLibrary/controller/AtorController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+
 import com.example.FilmLibrary.model.Ator;
 import com.example.FilmLibrary.repository.AtorRepository;
 
